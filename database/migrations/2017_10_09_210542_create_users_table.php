4dc9663b553cdbceb51bf80d35b48941f2754976
KobeVervoort <kobevervoort@gmail.com>
2017-10-10 10:56:02 +0200

    Added user table migration

--- database/migrations/2017_10_09_210542_create_users_table.php
+++ database/migrations/2017_10_09_210542_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('token');
+            $table->string('avatar');
+            $table->string('gender');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
     }
 
