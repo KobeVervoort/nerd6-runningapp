6295942f981f7c913c5e38943d57247c9953316e
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2017-10-11 11:27:42 +0200

    added api request (hardcoded)

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -18,8 +18,25 @@ class ActivitiesController extends Controller
     public function showAll()
     {
 
+        $code = request()->code;
+        // var_dump($code);
 
+        //$id = Auth::user()->id;
+        $token  = User::all()->where('id', 1);
+        var_dump($token);
+        // $token = Auth::User->token();
 
-        return view('activities');
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request( 'GET', 'https://www.strava.com/api/v3/athlete/activities/', [
+            'headers' => [
+                'Authorization' => 'Bearer f1bf048f75872dbe22fdeb146f821f4d54d93a54 ',
+            ]
+        ]);
+
+        $result = json_decode($res->getBody());
+
+        //var_dump($result);
+
+        //return view('activities');
     }
 }
