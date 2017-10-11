a0ce48bc540401175ac5cce02bb8767b21469dc8
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2017-10-11 14:19:39 +0200

    save activities to database

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -22,6 +23,9 @@ class ActivitiesController extends Controller
 
         //var_dump($token);
 
+        // Request the Strava API and get the athlete activities
+
+        // 1 keer in database steken en dan vanuit de database ophalen
         $client = new \GuzzleHttp\Client();
         $res = $client->request( 'GET', 'https://www.strava.com/api/v3/athlete/activities/', [
             'headers' => [
@@ -29,10 +33,29 @@ class ActivitiesController extends Controller
             ]
         ]);
 
-        $result = json_decode($res->getBody());
+        $res = json_decode($res->getBody());
 
-        //var_dump($result);
+        foreach ($res as $result) {
 
-        //return view('activities');
+            // Check if activity id already exists
+            $activityId = Activity::all()->where('activityId', $result->id)->first();
+
+            // Als activity id reeds bestaat in tabel --> niets
+            if ( $activityId === null)
+            {
+                $activity = new Activity;
+                $activity->name = $result->name;
+                $activity->activityId = $result->id;
+                $activity->stravaId = $result->athlete->id; // = strava_id in table Users
+                $activity->distance = $result->distance;
+                $activity->startDate = $result->start_date;
+                $activity->averageSpeed = $result->average_speed;
+                $activity->save();
+            }
+        }
+
+        var_dump($res);
+
+        //return view('activities')->with('activities', $activities);
     }
 }
---
+++ database/migrations/2017_10_11_100145_create_activities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activityId');
+            $table->integer('stravaId');
+            $table->string('name');
+            $table->integer('distance');
+            $table->string('startDate');
+            $table->float('averageSpeed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
