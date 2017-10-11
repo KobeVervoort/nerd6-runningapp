f15903edf8c74b85764d73a67421b6e8519e3503
KobeVervoort <kobevervoort@gmail.com>
2017-10-11 08:14:30 +0200

    Added login and home views

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/app.css" type="text/css">
+    <title>Welcome</title>
+</head>
+<body>
+
+    <div class="login">
+
+        <h1 class="login__title">Welcome to Nerd Running</h1>
+
+        <a class="login__button" href="/loginstrava">Connect with <img src="img/strava_logo.png" alt="Strava logo" class="login__logo"></a>
+
+    </div>
+</body>
+</html>
---
+++ resources/views/shared/nav.blade.php
@@ -0,0 +1,19 @@
+<nav class="nav">
+
+    <a href="" class="logo">Nerd Running</a>
+
+    <a href="" class="nav__link">item 1</a>
+    <a href="" class="nav__link">item 2</a>
+    <a href="" class="nav__link">item 3</a>
+    <a href="" class="nav__link">item 4</a>
+
+    <a href="" class="my-profile-link">
+
+        <p>John</p>
+        <p>Appleseed</p>
+
+        <img src="" alt="" class="my-profile-link__image">
+
+    </a>
+
+</nav>
