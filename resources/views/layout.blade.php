2a3a0061cd13ddcdfdca69b1e6301f99a9067ed1
KobeVervoort <kobevervoort@gmail.com>
2017-10-18 02:40:01 +0200

    Added main layout blade file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/app.css" type="text/css">
+    <title>Nerd Running</title>
+</head>
+<body>
+
+    <header class="header">
+
+        @include('shared.nav')
+
+        @yield('header-info')
+
+    </header>
+
+    <div class="main">
+
+        @yield('content')
+
+    </div>
+
+    @yield('footer')
+
+</body>
+</html>
