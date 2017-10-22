76fe0ac6eaec7319460cdcf26f4cb0c6dac37aa3
KobeVervoort <kobevervoort@gmail.com>
2017-10-22 14:59:16 +0200

    Added endDate to activity model and added explicit reference to foreign key in relationship

--- app/Activity.php
+++ app/Activity.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
+    protected $dates = array('endDate'); // tells laravel to make a Carbon object to whatever attribute we add to $dates
 
     public function user()
     {
 
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'userId');
 
     }
 
