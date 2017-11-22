4fa1161309a2f6a96b7e69beea314a598a36271a
KobeVervoort <kobevervoort@gmail.com>
2017-11-22 02:39:06 +0100

    Added schedule migration and model

---
+++ database/migrations/2017_11_21_235820_create_schedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('week');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('distance_goal');
+            $table->integer('distance_reached');
+            $table->integer('frequency_goal');
+            $table->integer('frequency_reached');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
