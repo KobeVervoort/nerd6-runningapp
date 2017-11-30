8a6e193d26087eb6e59915bddcd8b86118141b1b
KobeVervoort <kobevervoort@gmail.com>
2017-11-30 14:55:10 +0100

    Added groups to database seeder with one default group

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class)
+        factory(App\Group::class, 1)->create();
         factory(App\User::class, 20)->create();
         factory(App\Activity::class, 50)->create();
     }
