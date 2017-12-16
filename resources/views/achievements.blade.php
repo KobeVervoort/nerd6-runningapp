202664cb907df44945dd480a2888a8ad2a94530f
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2017-12-16 15:30:36 +0100

    Show the achievements which are not gratulated yet, but congratulate them immediately after

--- resources/views/achievements.blade.php
+++ resources/views/achievements.blade.php
@@ -35,11 +35,49 @@
 
 @section ('content')
 
+    <?php
+
+        $congratulateAchievements = App\AchievementUser::orderBy('created_at', 'desc')->where('user_id', '=' , auth()->user()->id)->where('congratulated', '=', 0)->get();
+
+        // Update congratulated=0 to congratulated=1
+        foreach ($congratulateAchievements as $congratulate){
+            $record = AchievementUser::find($congratulate->id);
+            $record->congratulated = 1;
+            $record->save();
+        }
+
+    ?>
+
     <h1 class="title">My Achievements</h1>
 
     @if($achievementsLoggedIn->isEmpty())
-        <img style="width:60%; height: auto;display:block; margin:0 auto;" src="https://memegenerator.net/img/images/600x600/11745649/run-forrest-run.jpg" alt="Run forrest run">
+        <img style="width:60%; height: auto;display:block; margin:0 auto;margin-bottom:20px;" src="https://memegenerator.net/img/images/600x600/11745649/run-forrest-run.jpg" alt="Run forrest run">
     @else
+
+        @if(count($congratulateAchievements) != 0)
+
+            <div class="congratulate-achievement-container" style="border: 2px solid green; background-color: lightgreen; border-radius: 5px; padding: 20px;">
+
+                <h2 style="font-size: 20px; font-weight: bold; color: white; margin-bottom: 20px;">You earned new achievements</h2>
+
+            @foreach($congratulateAchievements as $congratulate)
+
+                <div class="single-achievement" style="display: flex; justify-content: space-around;">
+
+                    <div><img src="/public/img/medal-run-blue.png" alt="blue medal"></div>
+                    <div>
+                        <p style="font-weight: bold;">1 run</p>
+                    </div>
+                    <div>{{ $congratulate->updated_at->diffForHumans() }}</div><!-- time ago -->
+
+                </div>
+
+            @endforeach
+
+            </div> <!-- end congratulate-achievement-container -->
+
+        @endif
+
 
         @foreach($achievementsLoggedIn as $achievement)
 
@@ -59,7 +97,7 @@
     <h1 class="title">Weekly Summaries</h1>
 
     @if($weeklyAchievements->isEmpty())
-        <img style="width:60%; height: auto;display:block; margin:0 auto;" src="https://memegenerator.net/img/images/600x600/11745649/run-forrest-run.jpg" alt="Run forrest run">
+        <img style="width:60%; height: auto;display:block; margin:0 auto; margin-bottom:20px;" src="https://memegenerator.net/img/images/600x600/11745649/run-forrest-run.jpg" alt="Run forrest run">
     @else
 
         @foreach($weeklyAchievements as $achievement)
